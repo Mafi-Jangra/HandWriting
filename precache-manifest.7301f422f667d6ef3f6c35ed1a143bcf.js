8a803781692165bbc8df80bebc385b632f1430bd
Mafi-Jangra <mafijangra2@gmail.com>
2026-05-10 15:43:26 +0530

    Updates

---
+++ precache-manifest.7301f422f667d6ef3f6c35ed1a143bcf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e56456b6a144ff163ca68fffd909c84e",
+    "url": "/HandWriting/index.html"
+  },
+  {
+    "revision": "11cf8c1af82732a91c27",
+    "url": "/HandWriting/static/css/main.e360b6b8.chunk.css"
+  },
+  {
+    "revision": "d2e57f6c79e5ad0905d7",
+    "url": "/HandWriting/static/js/2.979fe64b.chunk.js"
+  },
+  {
+    "revision": "4e4e0cc1781d9df56b5dd3d1dd940f36",
+    "url": "/HandWriting/static/js/2.979fe64b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11cf8c1af82732a91c27",
+    "url": "/HandWriting/static/js/main.1553bb3e.chunk.js"
+  },
+  {
+    "revision": "67efbb55de41fc4cfc96",
+    "url": "/HandWriting/static/js/runtime-main.7536d75b.js"
+  },
+  {
+    "revision": "61532e6edcc43e09007ab371f0671d37",
+    "url": "/HandWriting/static/media/convert.61532e6e.svg"
+  },
+  {
+    "revision": "9864526410d6a6547324356c7a268de7",
+    "url": "/HandWriting/static/media/footer.98645264.svg"
+  },
+  {
+    "revision": "b40b26736c54f0d65a1018ad8f91134d",
+    "url": "/HandWriting/static/media/pen.b40b2673.svg"
+  },
+  {
+    "revision": "922811086f5ce51cf92a1c0b28ad01dc",
+    "url": "/HandWriting/static/media/upload.92281108.svg"
+  }
+]);
